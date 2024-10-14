5cf99fdf2fc8076d1d8d216e872547d668fabdd6
Arpit Singhal <87632061+arpit84@users.noreply.github.com>
2024-10-14 09:14:09 +0530

    Update AppTest.java

--- src/test/java/com/app/AppTest.java
+++ src/test/java/com/app/AppTest.java
@@ -3,6 +3,7 @@ package com.app;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -10,24 +11,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest
 {
     @Test
-    public void testAppConstructor() {
+    public void testAppInstancesReturnSameMessage() {
         App app1 = new App();
         App app2 = new App();
-        assertEquals(app1.getMessage(), app2.getMessage());
+        assertEquals(app1.getMessage(), app2.getMessage(), "Messages from different App instances should be equal.");
     }
 
     @Test
-    public void testAppMessage()
-    {
+    public void testAppMessage() {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello World!", app.getMessage(), "The message should be 'Hello World!'");
     }
 
     @Test
-    public void testShouldAnswerTrue()
-    {
-        assertTrue( true );
+    public void testBasicTrueAssertion() {
+        assertTrue(true, "This test should always pass.");
     }
-
-
 }
